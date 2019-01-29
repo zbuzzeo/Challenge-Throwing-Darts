5768c68ca3fa18802d0b34ed40536ac3d271e5a4
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-29 09:26:11 -1000

    works fine with console.log() testing. need to write tests.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function scoreCalculator(arr) {
-  // do work here
+const scoreThrows = (array) => {
+  let points = 0;
+
+  // if all throws are < 5 then add 100 to points:
+  const lessThanFive = array.filter(x => { return x < 5 });
+
+  if (lessThanFive.length === array.length) {
+    points += 100;
+  }
+
+  const calcPoints = array.reduce((sum, x) => {
+    console.log(array);
+    if (x <= 10 && x >= 5) {
+      sum += 5;
+    } else if (x < 5) {
+      sum += 10;
+    }
+
+    return sum;
+  }, 0);
+
+  points += calcPoints;
+
+  return points;
 }
 
-module.exports = scoreCalculator;
+console.log(
+  scoreThrows([1, 2, 1, 4, 4, 2])
+  );
+
+module.exports = scoreThrows;
